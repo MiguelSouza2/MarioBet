e62978421f7420faecc9cd6a466c4cc1d627a2cf
2º ETIM - Desenvolvimento de Sistemas <ds-etim-2@servidor.acad>
2024-02-29 15:25:40 -0300

    Feito o projeto

--- js/termo.js
+++ js/termo.js
@@ -10,25 +10,24 @@ let letter_5 = document.getElementById("termo_input_5");
 
 function return_letter() {
   document.getElementById("termo_input_2").addEventListener("keydown", function (event) {
-    if (event.ctrlKey && event.key === "Backspace") {
+    if (event.key === "Backspace") {
       letter_1.focus();
-      console.log("returnizin");
     }
   });
   document.getElementById("termo_input_3").addEventListener("keydown", function (event) {
-    if (event.ctrlKey && event.key === "Backspace") {
+    if (event.key === "Backspace") {
       letter_2.focus();
-      console.log("returnizin");
     }
   });
   document.getElementById("termo_input_4").addEventListener("keydown", function (event) {
-    if (event.ctrlKey && event.key === "Backspace") {
+    if (event.key === "Backspace") {
       letter_3.focus();
     }
   });
   document.getElementById("termo_input_5").addEventListener("keydown", function (event) {
-    if (event.ctrlKey && event.key === "Backspace") {
+    if (event.key === "Backspace") {
       letter_4.focus();
+      letter_5.value = '';
     }
   });
 }
@@ -37,18 +36,14 @@ function return_letter() {
 function start_game() {
   if (letter_1.value !== "") {
     letter_2.focus();
-    console.log("hello");
   }
   if (letter_2.value !== "") {
     letter_3.focus();
-    console.log("hello");
   }
   if (letter_3.value !== "") {
     letter_4.focus();
-    console.log("hello");
   }
   if (letter_4.value !== "") {
     letter_5.focus();
-    console.log("hello");
   }
 }
