f81f828a57fb151003dbd9a4c0698f465bd24f7c
2º ETIM - Desenvolvimento de Sistemas <ds-etim-2@servidor.acad>
2024-02-29 11:58:24 -0300

    alguma coisa

--- js/termo.js
+++ js/termo.js
@@ -8,55 +8,47 @@ let letter_3 = document.getElementById("termo_input_3");
 let letter_4 = document.getElementById("termo_input_4");
 let letter_5 = document.getElementById("termo_input_5");
 
-
-function return_letter(){
-  letter_2.addEventListener("keydown", function(event) { 
-  if (event.ctrlKey && event.key === "Backspace") {
+function return_letter() {
+  document.getElementById("termo_input_2").addEventListener("keydown", function (event) {
+    if (event.ctrlKey && event.key === "Backspace") {
       letter_1.focus();
-  }
-})
-letter_3.addEventListener("keydown", function(event) {
-  if (event.ctrlKey && event.key === "Backspace") {
+      console.log("returnizin");
+    }
+  });
+  document.getElementById("termo_input_3").addEventListener("keydown", function (event) {
+    if (event.ctrlKey && event.key === "Backspace") {
       letter_2.focus();
-  }
-})
-letter_4.addEventListener("keydown", function(event) {
-  if (event.ctrlKey && event.key === "Backspace") {
+      console.log("returnizin");
+    }
+  });
+  document.getElementById("termo_input_4").addEventListener("keydown", function (event) {
+    if (event.ctrlKey && event.key === "Backspace") {
       letter_3.focus();
-  }
-})
-letter_5.addEventListener("keydown", function(event) {
-  if (event.ctrlKey && event.key === "Backspace") {
+    }
+  });
+  document.getElementById("termo_input_5").addEventListener("keydown", function (event) {
+    if (event.ctrlKey && event.key === "Backspace") {
       letter_4.focus();
-  }
-})
-} 
-
-
-function start_game() {
-
-  
-    if (letter_1.value !== "") {
-      letter_2.focus();
-      console.log("hello");
-
     }
-    if (letter_2.value !== "") {
-      letter_3.focus();
-      console.log("hello");
-    }
-    if (letter_3.value !== "") {
-      letter_4.focus();
-      console.log("hello");
-    }
-    if (letter_4.value !== "") {
-      letter_5.focus();
-      console.log("hello");
-    }
-
+  });
 }
 
 
-
-
-console.log("funfando");
+function start_game() {
+  if (letter_1.value !== "") {
+    letter_2.focus();
+    console.log("hello");
+  }
+  if (letter_2.value !== "") {
+    letter_3.focus();
+    console.log("hello");
+  }
+  if (letter_3.value !== "") {
+    letter_4.focus();
+    console.log("hello");
+  }
+  if (letter_4.value !== "") {
+    letter_5.focus();
+    console.log("hello");
+  }
+}
