d15117c2ce910721ed792c56aee2af03666e10b2
Miguel Isack verdadeiro <130250696+MiguelSouza2@users.noreply.github.com>
2024-03-02 01:14:24 -0300

    fiz o termo da mariobet

--- js/termo.js
+++ js/termo.js
@@ -1,4 +1,4 @@
-const lista_de_palavras = ["primo"];
+const lista_de_palavras = ["p", "r", "i", "m", "o"];
 
 let exibicao_palavra;
 
@@ -9,6 +9,7 @@ let letter_4 = document.getElementById("termo_input_4");
 let letter_5 = document.getElementById("termo_input_5");
 
 function return_letter() {
+
   document.getElementById("termo_input_2").addEventListener("keydown", function (event) {
     if (event.key === "Backspace") {
       letter_1.focus();
@@ -30,20 +31,40 @@ function return_letter() {
       letter_5.value = '';
     }
   });
+  
 }
 
 
 function start_game() {
+  input_letters = []
+  
   if (letter_1.value !== "") {
+    input_letters[0] = letter_1.value;
     letter_2.focus();
   }
   if (letter_2.value !== "") {
+    input_letters[1] = letter_2.value;
     letter_3.focus();
   }
   if (letter_3.value !== "") {
+    input_letters[2] = letter_3.value;
     letter_4.focus();
   }
   if (letter_4.value !== "") {
+    input_letters[3] = letter_4.value;
     letter_5.focus();
+  }
+  if(letter_5.value !== ""){
+    input_letters[4] = letter_5.value;
+  }
+
+  console.log(input_letters);
+  console.log(lista_de_palavras);
+  if(input_letters.length === lista_de_palavras.length){
+    letter_1.style.backgroundColor = 'green';
+    letter_2.style.backgroundColor = 'green';
+    letter_3.style.backgroundColor = 'green';
+    letter_4.style.backgroundColor = 'green';
+    letter_5.style.backgroundColor = 'green';
   }
 }
